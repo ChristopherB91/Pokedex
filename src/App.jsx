3c787bc541b0851a3b5993708f38fc91159863bf
Chris <chrisbrowne91@gmail.com>
2022-12-28 00:07:54 -0500

    api is done homepage next

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export default function App() {
+  const [data, setData] = useState();
+  const [favorite, setFavorite] = useState();
+  const [name, setName] = useState();
+  const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+
+  const searchPokemon = (event) => {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setName("");
+  }
+
+    console.log(data)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <button className="darkmode">DarkMode</button>
+      <input
+        type="text"
+        className="searchBar"
+        placeholder="Enter a pokemon name"
+        onChange={(event) => setName(event.target.value)}
+      />
+      <button className="search" onClick={searchPokemon}>
+        SEARCH
+      </button>
+      <button className="fav">fav</button>
+      <div className="title"><img src="https://fontmeme.com/permalink/221227/848321aef1fd96421ea6edde598ce4ce.png" alt="pokemon-font" border="0" /></div>
+      <button className="type1">type1</button>
+      <button className="type2">type2</button>
+      <div className="moves"></div>
+    </>
+  );
 }
-
-export default App
