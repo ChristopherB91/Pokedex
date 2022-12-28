81364252cf4527d0838c684e71cae0e44e0da3dc
Chris <chrisbrowne91@gmail.com>
2022-12-28 12:39:17 -0500

    HomePage finished color next

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import axios from 'axios';
-
+import axios from "axios";
+import "./App.css";
 
 export default function App() {
   const [data, setData] = useState();
@@ -9,32 +9,73 @@ export default function App() {
   const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
 
   const searchPokemon = (event) => {
+    if(event.key === 'Enter'){
       axios.get(url).then((response) => {
         setData(response.data);
         console.log(response.data);
       });
-      setName("");
-  }
-
-    console.log(data)
+    }
+    axios.get(url).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    });
+  };
 
   return (
     <>
-      <button className="darkmode">DarkMode</button>
-      <input
-        type="text"
-        className="searchBar"
-        placeholder="Enter a pokemon name"
-        onChange={(event) => setName(event.target.value)}
-      />
-      <button className="search" onClick={searchPokemon}>
-        SEARCH
-      </button>
-      <button className="fav">fav</button>
-      <div className="title"><img src="https://fontmeme.com/permalink/221227/848321aef1fd96421ea6edde598ce4ce.png" alt="pokemon-font" border="0" /></div>
-      <button className="type1">type1</button>
-      <button className="type2">type2</button>
-      <div className="moves"></div>
+      <div className="header">
+        <button className="darkmode">DarkMode</button>
+        <div className="search">
+          <input
+            type="text"
+            className="searchBar"
+            placeholder="Enter a pokemon name"
+            onChange={(event) => setName(event.target.value.toLowerCase())}
+            onKeyPress = {searchPokemon}
+          />
+          <button className="search" onClick={searchPokemon}>
+            SEARCH
+          </button>
+        </div>
+        <button className="fav">Favorite</button>
+      </div>
+      <div className="title">
+        <img
+          src="https://fontmeme.com/permalink/221227/848321aef1fd96421ea6edde598ce4ce.png"
+          alt="pokemon-font"
+          border="0"
+          className="titlePic"
+        />
+      </div>
+      <div className="types">
+        <button className="type1">Fire</button>
+        <button className="type2">Flying</button>
+      </div>
+      <div className="content">
+        <div className="imgs">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/6.png"
+            alt="pokemon shiny img"
+            className="picture-shiny"
+          />
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
+            alt="pokemon normal img"
+            className="picture"
+          />
+        </div>
+        <div className="moves">
+          <ul>
+            <li>| mega-punch |</li>
+            <li>| fire-punch |</li>
+            <li>| thunder-punch |</li>
+            <li>| scratch |</li>
+            <li>| swords-dance |</li>
+            <li>| cut |</li>
+            <li>| wing-attack |</li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
